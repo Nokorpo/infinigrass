e14bde6ee55788637e89c66e830dcf10bcdb47ca
antimundo <pablo.rubio.ru@gmail.com>
2024-03-10 14:21:30 +0100

    Add grass feedback

--- scenes/grass/grass.gd
+++ scenes/grass/grass.gd
@@ -6,19 +6,27 @@ var target_pull_distance = 2400
 var is_pulling := false
 var pulled_distance := 0.0
 var mouse_position_last_frame: Vector2
+var tween_scale
 
-func _input(_event):
+func _input(event):
+	if !is_pulling and event is InputEventMouseMotion:
+		event.position
+		if can_pull(event.position):
+			_on_mouse_entered()
+		else:
+			_on_mouse_exited()
 	if Input.is_action_just_pressed("click"):
 		var mouse_positon = get_viewport().get_mouse_position()
-		const PULL_MARGIN = 80
-		if (mouse_positon - position).length() < PULL_MARGIN:
+		if can_pull(mouse_positon):
 			is_pulling = true
 			pulled_distance = 0.0
 			mouse_position_last_frame = mouse_positon
+			$AnimationPlayer.stop()
 	elif Input.is_action_just_released("click"):
+		$AnimationPlayer.play("idle", 100)
 		is_pulling = false
 		modulate.r = 1
-		rotation = 0
+		#rotation = 0
 		scale.y = 1
 		scale.x = 1
 
@@ -40,3 +48,19 @@ func _process(_delta):
 		if pulled_distance > target_pull_distance:
 			pulled.emit()
 			queue_free()
+
+func can_pull(mouse_position: Vector2):
+	const PULL_MARGIN = 80
+	return (mouse_position - position).length() < PULL_MARGIN 
+
+func _on_mouse_entered():
+	if tween_scale != null:
+		tween_scale.kill()
+	tween_scale = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween_scale.tween_property(self, "scale", Vector2(1.1, 1.1), .05)
+
+func _on_mouse_exited():
+	if tween_scale != null:
+		tween_scale.kill()
+	tween_scale = create_tween().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween_scale.tween_property(self, "scale", Vector2(1, 1), .1)

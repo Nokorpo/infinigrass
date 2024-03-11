6f47dbd5db248185684226355da1e4811a2e78b6
antimundo <pablo.rubio.ru@gmail.com>
2024-03-11 17:03:29 +0100

    Fix wrong resource manager values

--- scenes/resources_manager.gd
+++ scenes/resources_manager.gd
@@ -9,9 +9,9 @@ signal beer_changed
 ## Called if player tries to buy something, but doesn't have enough of that resource
 signal not_enough(resource: GameResourceType)
 
-var money: int = 110
-var barley: int = 110
-var beer: int = 10
+var money: int = 0
+var barley: int = 0
+var beer: int = 0
 
 func add_resource(amount: int, resource_type: GameResourceType) -> void:
 	match resource_type:

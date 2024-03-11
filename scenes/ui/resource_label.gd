aa9f766af0f55e7157ab5d919fed708332f986e8
Juan Antonio Nepormoseno Rosales <nepormoseno1992@gmail.com>
2024-03-11 16:19:13 +0100

    Skip frames when changing resources

--- scenes/ui/resource_label.gd
+++ scenes/ui/resource_label.gd
@@ -1,9 +1,11 @@
 extends Label
 
 @export var resource: ResourcesManager.GameResourceType
+@export var UPDATE_EVERY_X_FRAMES: int = 5
 
 var real_value = 0 ## Real value of the resource
 var showing_value = 0 ## Value showed to the player right now
+var frame_number = 0
 
 func _ready() -> void:
 	match resource:
@@ -20,6 +22,9 @@ func update_text() -> void:
 
 func _physics_process(_delta):
 	if real_value != showing_value:
+		frame_number += 1
+		if frame_number % UPDATE_EVERY_X_FRAMES != 0:
+			return
 		if real_value > showing_value:
 			showing_value += 1
 		else:

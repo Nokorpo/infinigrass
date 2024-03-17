0cd90b7af2a4e1ada1454ffcab22a0e3a0b3609a
Juan Antonio Nepormoseno Rosales <nepormoseno1992@gmail.com>
2024-03-17 16:04:46 +0100

    Add catalan translation

--- localization/localization_button.gd
+++ localization/localization_button.gd
@@ -1,19 +1,21 @@
 extends Control
 
+const SUPPORTED_LANGUAGES = ["en", "es", "fr", "cat"]
+
 func _ready():
 	var current_language = TranslationServer.get_locale()
-	if current_language != "en"\
-		and current_language != "es"\
-		and current_language != "fr":
+	if current_language not in SUPPORTED_LANGUAGES:
 		TranslationServer.set_locale("es")
 
 func _on_toggle_language_pressed():
 	match TranslationServer.get_locale():
-		"es":
+		"cat":
 			TranslationServer.set_locale("en")
 		"en":
 			TranslationServer.set_locale("fr")
 		"fr":
 			TranslationServer.set_locale("es")
+		"es":
+			TranslationServer.set_locale("cat")
 		_:
 			TranslationServer.set_locale("es")
